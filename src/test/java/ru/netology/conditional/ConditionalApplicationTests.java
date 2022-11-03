294ef7a00e8fa2246a9e6e4e7e15bd1c10da5651
Maxya777 <max3532@yandex.ru>
2022-11-03 15:19:42 +0300

    Added Test

--- src/test/java/ru/netology/conditional/ConditionalApplicationTests.java
+++ src/test/java/ru/netology/conditional/ConditionalApplicationTests.java
@@ -1,13 +1,43 @@
 package ru.netology.conditional;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ConditionalApplicationTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private static final GenericContainer<?> devapp = new GenericContainer<>("devapp")
+            .withExposedPorts(8080);
+    private static final GenericContainer<?> prodapp = new GenericContainer<>("prodapp")
+            .withExposedPorts(8081);
+
+    @BeforeAll
+    public static void setUp() {
+        devapp.start();
+        prodapp.start();
+    }
 
     @Test
     void contextLoads() {
+
+        ResponseEntity<String> entityFromDevapp = restTemplate.
+                getForEntity("http://localhost:" + devapp.getMappedPort(8080) + "/profile", String.class);
+        System.out.println("devapp " + entityFromDevapp.getBody());
+        Assertions.assertEquals("Current profile is dev", entityFromDevapp.getBody());
+
+        ResponseEntity<String> entityFromProdapp = restTemplate.
+                getForEntity("http://localhost:" + prodapp.getMappedPort(8081) + "/profile", String.class);
+        System.out.println("prodapp " + entityFromProdapp.getBody());
+        Assertions.assertEquals("Current profile is production", entityFromProdapp.getBody());
     }
 
 }
